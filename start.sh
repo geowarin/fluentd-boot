55efa22f9b72d70524f99e77d802778d463418f9
geowarin <geowarin@gmail.com>
2016-01-11 13:26:01 +0100

    Simplify

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f compose/fluentd/docker-compose.yml up -d
-sleep 1
-docker-compose -f compose/app/docker-compose.yml up -d
--- stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f compose/app/docker-compose.yml kill
-docker-compose -f compose/fluentd/docker-compose.yml kill
